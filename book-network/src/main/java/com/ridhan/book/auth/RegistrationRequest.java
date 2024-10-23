7105afa66e113556b04f31268e3167dd2fa2ec2b
RidhanParvendhan <ridhanparvendhan@gmail.com>
2024-10-23 09:19:29 +0300

    User registration / security / app config Implementation

--- book-network/src/main/java/com/ridhan/book/auth/RegistrationRequest.java
+++ book-network/src/main/java/com/ridhan/book/auth/RegistrationRequest.java
@@ -1,12 +1,12 @@
 package com.ridhan.book.auth;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
 @Setter
